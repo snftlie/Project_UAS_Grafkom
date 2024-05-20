6d45e0e724f4d81b6aa3dd0301a5194efcfa5f47
fellllll <c14220027@john.petra.ac.id>
2024-05-20 17:12:40 +0700

    object person

--- threejs/main.js
+++ threejs/main.js
@@ -1,12 +1,12 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 // import WebGPU from 'three/addons/capabilities/WebGPU.js';
 // import WebGL from 'three/addons/capabilities/WebGL.js';
 
 // import WebGPURenderer from 'three/addons/renderers/webgpu/WebGPURenderer.js';
-
-
+const clock = new THREE.Clock();
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -14,46 +14,51 @@ document.body.appendChild(renderer.domElement);
 
 //setup scene and camera
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-camera.position.set(0,0,100); // set posisi camera tp blm arah hadapnya 
-camera.lookAt(0,0,0); // set posisi kamera menghadap 0,0,0 (rotasi cameranya)
+camera.position.set(0, 0, 100); // set posisi camera tp blm arah hadapnya
+camera.lookAt(0, 0, 0); // set posisi kamera menghadap 0,0,0 (rotasi cameranya)
 
 // Orbit Control
-var controls = new OrbitControls(camera,renderer.domElement);
-controls.target.set(0,5,0);
+var controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 5, 0);
 // controls.minDistance = 0.2;
 // 				controls.maxDistance = 10;
 controls.update();
 
-// Light 
+// Light
 // Directional Light
-var light = new THREE.DirectionalLight(0xFFFFFF,0.5);
-light.position.set(0,10,0);
-light.target.position.set(-5,0,0);
+var light = new THREE.DirectionalLight(0xffffff, 0.5);
+light.position.set(0, 10, 0);
+light.target.position.set(-5, 0, 0);
 scene.add(light);
 scene.add(light.target);
 
-// HEMISPHERE LIGHT => biar kyk matahari bs mantulno cahaya kann trs kita bs liat langitnya biru 
-light = new THREE.HemisphereLight(0xB1E1FF,0xB97A20,0.5);
+// HEMISPHERE LIGHT => biar kyk matahari bs mantulno cahaya kann trs kita bs liat langitnya biru
+light = new THREE.HemisphereLight(0xb1e1ff, 0xb97a20, 0.5);
 scene.add(light);
 
 // POINT LIGHT
-light = new THREE.PointLight(0xFFFF00,50);
-light.position.set(0,10,0);
+light = new THREE.PointLight(0xffff00, 50);
+light.position.set(0, 10, 0);
 scene.add(light);
 
-// SPOT LIGHT 
-light = new THREE.SpotLight(0xFF0000,50);
-light.position.set(10,10,0);
+// SPOT LIGHT
+light = new THREE.SpotLight(0xff0000, 50);
+light.position.set(10, 10, 0);
 scene.add(light);
 
 // AMBIENT
-light = new THREE.AmbientLight( 0xFFFFFF,1); // soft white light
-scene.add( light );
-//buat kubus ijo muter" sama ada line bentuk segitiga sama kaki tanpa alas 
-//geometry 
-// const points = 
+light = new THREE.AmbientLight(0xffffff, 1); // soft white light
+scene.add(light);
+//buat kubus ijo muter" sama ada line bentuk segitiga sama kaki tanpa alas
+//geometry
+// const points =
 // points.push(new THREE.Vector3(-1,0,0));
 // points.push(new THREE.Vector3(0,1,0));
 // points.push(new THREE.Vector3(1,0,0));
@@ -63,14 +68,12 @@ scene.add( light );
 // var line = new THREE.Line(geometry, material);
 // scene.add(line);
 
-
 // var geometry = new THREE.BoxGeometry(1,1,1); // lebar, tinggi, kedalaman // buat bikin objectnya
-// var material = new THREE.MeshBasicMaterial({color: 0x00FF00}); // buat bikin shadernya 
+// var material = new THREE.MeshBasicMaterial({color: 0x00FF00}); // buat bikin shadernya
 // var cube = new THREE.Mesh(geometry, material);
 // scene.add(cube);
 
-
-// cube merah 
+// cube merah
 // var points_custom = [-1,-1,1,1,-1,1,-1,1,1,-1,1,1,1,-1,1,1,1,1,1,-1,1,1,-1,-1,1,1,1,1,1,1,1,-1,-1,1,1,-1,1,-1,-1,-1,-1,-1,1,1,-1,1,1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,1,-1,-1,1,-1,-1,-1,1,-1,1,1,1,1,-1,-1,1,-1,1,1,1,1,1,1,-1,1,-1,-1,1,1,1,-1,1,-1,-1,1,1,-1,-1,1,-1,-1,-1,-1,1,-1,-1,-1];
 // var geometry = new THREE.BufferGeometry();
 // geometry.setAttribute(
@@ -84,7 +87,7 @@ scene.add( light );
 
 // // geometry
 // const object = [];
-// // plane -> one side, kalo yg dibuat di uts itu two side 
+// // plane -> one side, kalo yg dibuat di uts itu two side
 // {
 // var planeGeo = new THREE.PlaneGeometry(40,40);
 // var planeMat = new THREE.MeshPhongMaterial({
@@ -119,13 +122,54 @@ scene.add( light );
 // scene.add(mesh);
 // }
 
-const onProgress = function ( xhr ) {
-  if ( xhr.lengthComputable ) {
-
-    const percentComplete = xhr.loaded / xhr.total * 100;
-    console.log( percentComplete.toFixed( 2 ) + '% downloaded' );
+const onProgress = function (xhr) {
+  if (xhr.lengthComputable) {
+    const percentComplete = (xhr.loaded / xhr.total) * 100;
+    console.log(percentComplete.toFixed(2) + "% downloaded");
   }
 };
+
+// CHARACTER FBX
+const a = new Map(); // Walk, Run, Idle
+
+const loader1 = new FBXLoader();
+let mixer;
+loader1.load("Walking.fbx", function (object) {
+  mixer = new THREE.AnimationMixer(object);
+
+  const action = mixer.clipAction(object.animations[0]);
+  a.set("walk", mixer.clipAction(object.animations[0]));
+  action.play();
+
+  object.traverse(function (child) {
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+
+  object.position.set(5, 0, 0);
+  object.scale.setScalar(0.1);
+  scene.add(object);
+});
+
+loader1.load("Breathing Idle.fbx", function (object) {
+  mixer = new THREE.AnimationMixer(object);
+
+  const action = mixer.clipAction(object.animations[0]);
+  a.set("idle", mixer.clipAction(object.animations[0]));
+  action.play();
+
+  object.traverse(function (child) {
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+
+  object.scale.setScalar(0.1);
+  scene.add(object);
+});
 
 // new MTLLoader()
 //      .setPath( 'resources/' )
@@ -139,13 +183,13 @@ const onProgress = function ( xhr ) {
 //        .load( 'magic_book_OBJ.obj', function ( object ) {
 
 //         // object.position.y = - 0.95; // buat ganti posisi
-//         // object.scale.setScalar( 0.01 ); // buat scaling 
+//         // object.scale.setScalar( 0.01 ); // buat scaling
 //         scene.add( object );
 
 //        }, onProgress );
 
 //      } );
-const loader = new GLTFLoader(); 
+const loader = new GLTFLoader();
 // loader.setPath( 'coba_aset/' );//COBA
 // loader.load( 'scene.gltf',function ( gltf ) {
 //   const model = gltf.scene;
@@ -153,43 +197,42 @@ const loader = new GLTFLoader();
 //   renderer.compileAsync( model, camera, scene );
 //   scene.add( model );
 // } );
-loader.setPath( 'export_unreal/' );
-loader.load( 'EXPORT.gltf',function ( gltf ) {
-  const model = gltf.scene;
-  // wait until the model can be added to the scene without blocking due to shader compilation
-  renderer.compileAsync( model, camera, scene );
-  scene.add( model );
-} );
-loader.load('asset2/asset2.gltf',function ( gltf ) {
-  const model = gltf.scene;
-  // wait until the model can be added to the scene without blocking due to shader compilation
-  renderer.compileAsync( model, camera, scene );
-  scene.add( model );
-} );
-// loader.setPath( 'export_unreal/asset3' );
-loader.load( 'asset3/asset3.gltf',function ( gltf ) {
-  const model = gltf.scene;
-  // wait until the model can be added to the scene without blocking due to shader compilation
-  renderer.compileAsync( model, camera, scene );
-  scene.add( model );
-} );
-// loader.setPath( 'export_unreal/asset4' );
-loader.load( 'asset4/asset4.gltf',function ( gltf ) {
-  const model = gltf.scene;
-  // wait until the model can be added to the scene without blocking due to shader compilation
-  renderer.compileAsync( model, camera, scene );
-  scene.add( model );
-} );
+// loader.setPath("export_unreal/");
+// loader.load("EXPORT.gltf", function (gltf) {
+//   const model = gltf.scene;
+//   // wait until the model can be added to the scene without blocking due to shader compilation
+//   renderer.compileAsync(model, camera, scene);
+//   scene.add(model);
+// });
+// loader.load("asset2/asset2.gltf", function (gltf) {
+//   const model = gltf.scene;
+//   // wait until the model can be added to the scene without blocking due to shader compilation
+//   renderer.compileAsync(model, camera, scene);
+//   scene.add(model);
+// });
+// // loader.setPath( 'export_unreal/asset3' );
+// loader.load("asset3/asset3.gltf", function (gltf) {
+//   const model = gltf.scene;
+//   // wait until the model can be added to the scene without blocking due to shader compilation
+//   renderer.compileAsync(model, camera, scene);
+//   scene.add(model);
+// });
+// // loader.setPath( 'export_unreal/asset4' );
+// loader.load("asset4/asset4.gltf", function (gltf) {
+//   const model = gltf.scene;
+//   // wait until the model can be added to the scene without blocking due to shader compilation
+//   renderer.compileAsync(model, camera, scene);
+//   scene.add(model);
+// });
 
-
-// sun 
+// sun
 // var geometry = new THREE.SphereGeometry(1,12,3);
 // var material = new THREE.MeshBasicMaterial({color:0xffff00});
 // var sun = new THREE.Mesh(geometry, material);
 // scene.add(sun);
 // object.push(sun);
 
-// // earth 
+// // earth
 // geometry = new THREE.SphereGeometry(0.33,12,3);
 // material = new THREE.MeshBasicMaterial({color:0x00aaff});
 // var earth = new THREE.Mesh(geometry, material);
@@ -211,15 +254,18 @@ loader.load( 'asset4/asset4.gltf',function ( gltf ) {
 // earth.add(moon);
 
 var time_prev = 0;
-function animate(time){
+function animate(time) {
   var dt = time - time_prev;
   dt *= 0.1;
 
   // cube.rotation.x += 0.01 * dt;
   // cube.rotation.y += 0.01 * dt;
   // object.forEach((obj)=>{obj.rotation.z += dt * 0.01});
+  const delta = clock.getDelta();
 
-  renderer.render(scene,camera);
+  if (mixer) mixer.update(delta);
+
+  renderer.render(scene, camera);
 
   time_prev = time;
   requestAnimationFrame(animate);
